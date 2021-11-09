67dcd1a68549520ef669fabb7d9aa4b020cda4f7
hotjavabeans <james-ware@hotmail.co.uk>
2021-11-09 17:05:52 +0000

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("customers")]
+        public ActionResult Customers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "James Ware" },
+                new Customer { Name = "Beth Moignard" },
+                new Customer { Name = "Nyx" }
+            };
+
+            var viewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "12 Monkeys" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "James Ware" },
+                new Customer { Name = "Beth Moignard" },
+                new Customer { Name = "Nyx" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model
+
+            //ViewData["RandomMovie"] = movie;
+            //ViewBag.Movie = movie; runtime, no compile-time safety
+
+            //return new ViewResult(); less common method of returning a View. Best practice: use View helper method.
+            //return Content("Hello World!"); plain string content
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
