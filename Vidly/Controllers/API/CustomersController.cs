e41da2978b16e17e7ac99efee7441007410243a7
hotjavabeans <james-ware@hotmail.co.uk>
2021-11-16 20:10:20 +0000

    Add Movie MappingProfile. Add CustomerApiController. Add CustomerDto. Add MovieApiController. Add MovieDto.

--- Vidly/Controllers/API/CustomersController.cs
+++ Vidly/Controllers/API/CustomersController.cs
@@ -25,22 +25,22 @@ namespace Vidly.Controllers.API
         }
 
         // GET /api/customers/id 
-        public CustomerDto GetCustomer(int id)
+        public IHttpActionResult GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+                NotFound();
 
-            return Mapper.Map<Customer, CustomerDto>(customer);
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
         }
 
         // POST /api/customers
         [HttpPost]
-        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                return BadRequest();
 
             var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
@@ -48,7 +48,7 @@ namespace Vidly.Controllers.API
 
             customerDto.Id = customer.Id;
 
-            return customerDto;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
         }
 
         // PUT /api/customers/1
--- Vidly/Dtos/MovieDto.cs
+++ Vidly/Dtos/MovieDto.cs
@@ -7,7 +7,7 @@ using Vidly.Models;
 
 namespace Vidly.Dtos
 {
-    public class CustomerDto
+    public class MovieDto
     {
         public int Id { get; set; }
 
@@ -15,11 +15,13 @@ namespace Vidly.Dtos
         [StringLength(255)]
         public string Name { get; set; }
 
-        public bool IsSubscribedToNewsletter { get; set; }
- 
-        public byte MembershipTypeId { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
 
-        //[Min18YearsIfAMember]
-        public DateTime? Birthdate { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public byte NumberInStock { get; set; }
     }
 }
