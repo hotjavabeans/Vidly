968ae0db31b03960f626db709a65ac8646ab4ef4
hotjavabeans <james-ware@hotmail.co.uk>
2021-12-01 13:00:59 +0000

    Add Rentals table to Database. Add NewRentals Web Controller. Add RentalsController. Add NewRentalDto. Add New Rental View. Add Glimpse.

---
+++ Vidly/Models/Rental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Rental // Association class
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Customer Customer { get; set; } // navigation property
+
+        [Required]
+        public Movie Movie { get; set; } // navigation property
+
+        public DateTime DateRented { get; set; }
+
+        public DateTime? DateReturned { get; set; }
+    }
+}
